a9e3dfb65cede6ad15e73894ae65bc4002f0b802
Aleksandr Markin <alexnightmark@gmail.com>
2025-05-20 17:58:44 +0300

    initial tg webhook configuration

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # aiogram_webhook_test
+
+run nginx:
+`<em>`docker run --rm -d --name webhook-nginx -p 8080:8080 -v C:/Alex/Programming/projects/aiogram_webhook_test/nginx:/etc/nginx/conf.d nginx`</em>`
---
+++ config/web_cfg.py
@@ -0,0 +1,3 @@
+WEB_SERVER_HOST = "127.0.0.1"    # bind localhost only to prevent any external access   
+WEB_SERVER_PORT = 9000    # Port for incoming request from reverse proxy. Should be any available port
+WEBHOOK_PATH = "/webhook"    # Path to webhook route, on which Telegram will send requests
